13fd9eb31614b3750321051bb458ff1b72e1a175
Deepak Mishra <deepakbhardwajvst@gmail.com>
2024-04-10 14:35:08 +0530

    review and earn section completed

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["assets-global.website-files.com"],
+  },
+};
 
 export default nextConfig;
+
--- src/UiComponent/ReviewAndEarn/ReviewAndEarn.tsx
+++ src/UiComponent/ReviewAndEarn/ReviewAndEarn.tsx
@@ -1,103 +1,133 @@
+import React from "react";
+import "./ReviewAndEarn.css"; // Import the CSS file
 import Image from "next/image";
 
-const ReviewAndEarn: React.FC = () => {
+const ReviewAndEarn = () => {
   return (
-    <div style={{ marginTop: "1rem" }}>
-      <h5>Review & Earn</h5>
-      <div
-        style={{ padding: "1rem", boxShadow: "0px 1px 5px rgba(0, 0, 0, 0.1)" }}
-      >
-        <div style={{ display: "flex", alignItems: "center" }}>
-          <button style={{ marginRight: "0.5rem" }}>
-            <Image
-              width={500}
-              height={500}
-              priority={true}
-              src=""
-              style={{width:"24px",height:"24px"}}
-              alt="G2 Icon"
-            />
-          </button>
-          <div>
-            <p>Free 1-month</p>
-            <p>Growth Plan</p>
-            <p>Write an honest review of Scalenut on G2</p>
-          </div>
+    <div className="review-container">
+      <div className="re-box ">
+        <div className="re-heading">
+          <h5 className="long-form-title">Review & Earn</h5>
         </div>
-      </div>
-      <div
-        style={{ padding: "1rem", boxShadow: "0px 1px 5px rgba(0, 0, 0, 0.1)" }}
-      >
-        <div style={{ display: "flex", alignItems: "center" }}>
-          <button style={{ marginRight: "0.5rem" }}>
-            <Image
-              width={500}
-              height={500}
-              priority={true}
-              src=""
-              style={{width:"24px",height:"24px"}}
-              alt="Capterra Icon"
-            />
-          </button>
-          <div>
-            <p>Free 1-month</p>
-            <p>Growth Plan</p>
-            <p>Write an honest review of Scalenut on Capterra</p>
-          </div>
-        </div>
-      </div>
 
-      <h5 style={{ marginTop: "1rem" }}>Create Long-Form Content</h5>
-      <div
-        style={{ padding: "1rem", boxShadow: "0px 1px 5px rgba(0, 0, 0, 0.1)" }}
-      >
-        <div style={{ display: "flex", alignItems: "center" }}>
-          <button style={{ marginRight: "0.5rem" }}>
-            <Image
-              width={500}
-              height={500}
-              priority={true}
-              src=""
-              style={{width:"24px",height:"24px"}}
-              alt="Blog Icon"
-            />
-          </button>
-          <div>
-            <p>Free 2-months</p>
-            <p>Growth Plan</p>
-            <p>
-              Write a blog reviewing Scalenut, highlighting its features and
-              your personal experience
-            </p>
+        <div className="review-card">
+          <div className="review-item">
+            <div className="review-content">
+              <Image
+                src="https://assets-global.website-files.com/60ef088dd8fef919efabb412/636bb72d5fe195093fc8ddb5_g2-seeklogo.com.svg"
+                alt=""
+                width={500}
+                height={500}
+                className="review-image"
+                priority
+              />
+
+              <p className="plan-text">Free</p>
+
+              <p className="free-text">1-month</p>
+
+              <p className="plan-text p12">Growth Plan</p>
+
+              <p className="review-text">
+                Write an honest review of Scalenut on{" "}
+                <a href="">
+                  <strong>
+                    <u>G2</u>
+                  </strong>
+                </a>
+              </p>
+            </div>
+          </div>
+
+          <div className="review-item">
+            <div className="review-content">
+              <Image
+                src="https://assets-global.website-files.com/60ef088dd8fef919efabb412/63be6ab5ee5732ec1fe19bc9_Capterra.svg"
+                alt=""
+                width={500}
+                height={500}
+                className="review-image"
+                priority
+              />
+              <p className="plan-text">Free</p>
+              <p className="free-text">Free 1-month</p>
+              <p className="plan-text p12">Growth Plan</p>
+              <p className="review-text">
+                Write an honest review of Scalenut on{" "}
+                <a href="">
+                  <strong>
+                    <u>Capterra</u>
+                  </strong>
+                </a>
+              </p>
+            </div>
           </div>
         </div>
       </div>
-      <div
-        style={{ padding: "1rem", boxShadow: "0px 1px 5px rgba(0, 0, 0, 0.1)" }}
-      >
-        <div style={{ display: "flex", alignItems: "center" }}>
-          <button style={{ marginRight: "0.5rem" }}>
-            <Image
-              width={500}
-              height={500}
-              priority={true}
-              src=""
-              style={{width:"24px",height:"24px"}}
-              alt="Video Icon"
-            />
-          </button>
-          <div>
-            <p>Free 2-month</p>
-            <p>Growth Plan</p>
-            <p>
-              Create a Youtube video reviewing Scalenut, walk-through of its
-              features and functions
-            </p>
+      <div className="re-box ">
+        <div className="re-heading">
+          <h5 className="long-form-title">Review & Earn</h5>
+        </div>
+
+        <div className="review-card">
+          <div className="review-item">
+            <div className="review-content">
+              <Image
+                src="https://assets-global.website-files.com/60ef088dd8fef919efabb412/63be70efee57322a20e1e46f_Blog.svg"
+                alt=""
+                width={500}
+                height={500}
+                className="review-image"
+                priority
+              />
+
+              <p className="plan-text">Free</p>
+
+              <p className="free-text">1-month</p>
+
+              <p className="plan-text p12">Growth Plan</p>
+
+              <p className="review-text">
+                Write a{" "}
+                <a href="">
+                  <strong>
+                    <u>blog reviewing Scalenut</u>
+                  </strong>
+                </a>
+                , highlighting its features and your personal experience
+              </p>
+            </div>
+          </div>
+
+          <div className="review-item">
+            <div className="review-content">
+              <Image
+                src="https://assets-global.website-files.com/60ef088dd8fef919efabb412/636bb72d5fe195876cc8dda4_icons8-youtube-96.webp"
+                alt=""
+                width={500}
+                height={500}
+                className="review-image"
+                priority
+              />
+              <p className="plan-text">Free</p>
+              <p className="free-text">Free 1-month</p>
+              <p className="plan-text p12">Growth Plan</p>
+              <p className="review-text">
+                Create a{" "}
+                <a href="">
+                  <strong>
+                    <u>Youtube video</u>
+                  </strong>
+                </a>
+                Youtube video reviewing Scalenut, walk-through of its features
+                and functions
+              </p>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 };
 
 export default ReviewAndEarn;
