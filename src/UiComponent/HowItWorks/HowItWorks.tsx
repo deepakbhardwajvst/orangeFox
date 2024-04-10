9f7f50d7365a6c4a998c2c7e8addbe777805890f
Deepak Mishra <deepakbhardwajvst@gmail.com>
2024-04-10 15:31:56 +0530

    how it works section done

--- src/UiComponent/HowItWorks/HowItWorks.tsx
+++ src/UiComponent/HowItWorks/HowItWorks.tsx
@@ -1,37 +1,73 @@
 import React from "react";
 import styles from "./HowItWorks.module.css";
+import Image from "next/image";
 
 const HowItWorks: React.FC = () => {
   return (
     <div className={styles.container}>
-      <h2>How It Works</h2>
-      <div className={styles.stepContainer}>
-        <div className={styles.step}>
-          <div className={styles.stepIcon}>01</div>
-          <h3>Create</h3>
-          <p>
-            Post reviews, create content and share about-out in your social
-            media
-          </p>
-        </div>
-        <div className={styles.step}>
-          <div className={styles.stepIcon}>02</div>
-          <h3>Submit</h3>
-          <p>
-            Submit the published URLs with us via chat or email us at
-            wecare@scalenut.com
-          </p>
-        </div>
-        <div className={styles.step}>
-          <div className={styles.stepIcon}>03</div>
-          <h3>Earn</h3>
-          <p>
-            We will upgrade your account to the Growth Plan after verification.
-          </p>
+      <div className="">
+        <h2 className={styles.headig}>How It Works</h2>
+        <div className={styles.stepContainer}>
+          <div className={styles.step}>
+            <div className={styles.topbox}>
+              <div className={styles.stepIcon}>01</div>
+              <Image
+                src="https://assets-global.website-files.com/60ef088dd8fef919efabb412/63be724e4823669d982f5e6a_Create.svg"
+                width={500}
+                height={500}
+                priority
+                className={styles.image}
+                alt="step image"
+              />
+            </div>
+            <h3>Create</h3>
+            <p>
+              Post <strong>reviews</strong>, create <strong>content</strong> and share <strong>sbout-out</strong> on your social
+              media
+            </p>
+          </div>
+
+          <div className={styles.step}>
+            <div className={styles.topbox}>
+              <div className={styles.stepIcon}>02</div>
+              <Image
+                src="https://assets-global.website-files.com/60ef088dd8fef919efabb412/63be724e4823669d982f5e6a_Create.svg"
+                width={500}
+                height={500}
+                priority
+                className={styles.image}
+                alt="step image"
+              />
+            </div>
+            <h3>Submit</h3>
+            <p>
+              Submit the <strong>published URLs</strong> with us via chat or email us at
+              <a href="/" className={styles.email}>wecare@scalenut.com</a>
+            </p>
+          </div>
+
+          <div className={styles.step}>
+            <div className={styles.topbox}>
+              <div className={styles.stepIcon}>03</div>
+              <Image
+                src="https://assets-global.website-files.com/60ef088dd8fef919efabb412/63be724e4823669d982f5e6a_Create.svg"
+                width={500}
+                height={500}
+                priority
+                className={styles.image}
+                alt="step image"
+              />
+            </div>
+            <h3>Earn</h3>
+            <p>
+              We will upgrade your account to the Growth Plan after
+              verification.
+            </p>
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 };
 
 export default HowItWorks;
