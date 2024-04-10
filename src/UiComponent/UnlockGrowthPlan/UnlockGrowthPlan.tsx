f1d1c31c94f72ffe67173031b9e0095aa4e03185
Deepak Mishra <deepakbhardwajvst@gmail.com>
2024-04-10 18:07:42 +0530

    added best practice of tsx in unlock section

--- src/UiComponent/UnlockGrowthPlan/UnlockGrowthPlan.tsx
+++ src/UiComponent/UnlockGrowthPlan/UnlockGrowthPlan.tsx
@@ -1,13 +1,45 @@
-"use client";
+"use client"
 import React, { useState } from "react";
 import styles from "./UnlockGrowthPlan.module.css";
+
+interface FaqItem {
+  question: string;
+  answer: string;
+}
+
 const UnlockGrowthPlan: React.FC = () => {
-  // State to manage which items are expanded
+  const faqData: FaqItem[] = [
+    {
+      question: "Who is eligible for the Earn Credits Program?",
+      answer: "All our registered users are eligible to participate",
+    },
+    {
+      question: "Who is eligible for giving us reviews under this program?",
+      answer: "Answer to the second question...",
+    },
+    {
+      question: "How much can I earn through Scalenut's Earn Credits Program?",
+      answer: "Answer to the third question...",
+    },
+    {
+      question: "How will I get these rewards?",
+      answer: "Answer to the fourth question...",
+    },
+    {
+      question:
+        "What kind of blogs and videos will be eligible for free Growth Plan access?",
+      answer: "Answer to the fifth question...",
+    },
+    {
+      question: "Other Terms & Conditions",
+      answer: "Answer to the sixth question...",
+    },
+  ];
+
   const [expandedItems, setExpandedItems] = useState<boolean[]>(
-    new Array(6).fill(false)
+    new Array(faqData.length).fill(false)
   );
 
-  // Function to toggle the expansion of an item
   const toggleItem = (index: number) => {
     const newExpandedItems = [...expandedItems];
     newExpandedItems[index] = !newExpandedItems[index];
@@ -45,33 +77,6 @@ const UnlockGrowthPlan: React.FC = () => {
   );
 };
 
-// FAQ data
-const faqData = [
-  {
-    question: "Who is eligible for the Earn Credits Program?",
-    answer: "All our registered users are eligible to participate",
-  },
-  {
-    question: "Who is eligible for giving us reviews under this program?",
-    answer: "Answer to the second question...",
-  },
-  {
-    question: "How much can I earn through Scalenut's Earn Credits Program?",
-    answer: "Answer to the third question...",
-  },
-  {
-    question: "How will I get these rewards?",
-    answer: "Answer to the fourth question...",
-  },
-  {
-    question:
-      "What kind of blogs and videos will be eligible for free Growth Plan access?",
-    answer: "Answer to the fifth question...",
-  },
-  {
-    question: "Other Terms & Conditions",
-    answer: "Answer to the sixth question...",
-  },
-];
-
 export default UnlockGrowthPlan;
+
+
